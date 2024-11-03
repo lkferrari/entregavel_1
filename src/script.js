043f2a114d4f73b88ddd0df2f3618eaef6cc7c96
Ferrari <lucasferrari292@gmail.com>
2024-11-02 23:10:27 -0300

    Adicionada nova função de calculadora

--- src/script.js
+++ src/script.js
@@ -1,20 +1,54 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-//ESCREVA SEU CÓDIGO AQUI!!
+function Calculadora(n1, n2, operador) {
+  let resultado = 0
+  var num1 = Number(n1)
+  var num2 = Number(n2)
 
+  switch (operador) {
+    case '+':
+      resultado = num1 + num2
+      break
+
+    case '-':
+      resultado = num1 - num2
+      break
+
+    case '*':
+      resultado = num1 * num2
+      break
+
+    case '/':
+      resultado = num1 / num2
+      break
+
+    case 'e':
+      resultado = 1
+      for (let i = 0; i < num2; i++) {
+        resultado = resultado * num1
+      }
+      break
+  }
+
+  if ((resultado == undefined) | (resultado > 1000000)) {
+    return 'ERRO'
+  }
+
+  return resultado
+}
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-let visor = document.getElementById("visor");
+let visor = document.getElementById('visor')
 
 function adicionar(tecla) {
-  visor.value = visor.value + tecla;
+  visor.value = visor.value + tecla
 }
 
 function limpezatotal() {
-  visor.value = "";
+  visor.value = ''
 }
 
 function calcular() {
@@ -22,9 +56,8 @@ function calcular() {
   let operadores = visor.value.split(/[0-9]/)
   let operador
   operadores.forEach(x => {
-    if (x != "")
-      operador = x
-  });
+    if (x != '') operador = x
+  })
 
   visor.value = Calculadora(numeros[0], numeros[1], operador)
 }
