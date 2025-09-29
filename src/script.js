c03f9550073745c1011413011d6fdb3ca1274fa5
Lucas Ferrari <lucasferrari292@gmail.com>
2025-09-29 15:39:25 -0300

    Entrega calculadora

--- src/script.js
+++ src/script.js
@@ -3,6 +3,51 @@
 
 //ESCREVA SEU CÓDIGO AQUI!!
 
+function Calculadora (numero1, numero2, operador) {
+
+  let num1 = parseFloat(numero1)
+  let num2 = parseFloat(numero2)
+  let resultado
+
+  switch (operador){
+    case "+":
+      resultado = num1 + num2
+      break;
+    case "-":
+      resultado = num1 - num2
+      break;
+    case "*":
+      resultado = num1 * num2
+      break;
+    case "/":
+      resultado = num1 / num2
+      break;
+    case "e":
+      if (num2 === 0){
+        resultado = 1
+        break;
+      } else if (num2 === 1){
+        resultado = num1
+        break;
+      } else {
+        let i = 0
+        resultado = 1
+        while (i < num2){
+          resultado = resultado * num1
+          
+          i++
+        }
+      }
+      break;
+  }
+
+  if (resultado === "undefined" || resultado > 1000000){
+    resultado = "ERRO"
+  }
+
+  return resultado
+}
+
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
